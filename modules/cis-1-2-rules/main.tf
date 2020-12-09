161a804d53f0914abcb695949bd1ae9e1898ae83
Matt Calhoun <matt@mattcalhoun.com>
2020-12-08 19:11:28 -0500

    rename var to be generic

--- modules/cis-1-2-rules/main.tf
+++ modules/cis-1-2-rules/main.tf
@@ -2,11 +2,11 @@ locals {
   file_rules      = module.aws_config_rules_yaml_config.map_configs
   rules_with_tags = { for k, rule in local.file_rules : k => rule if rule.tags != null }
 
-  cis_1_2_tag      = "compliance/cis-aws-foundations/1.2"
-  logging_only_tag = "compliance/cis-aws-foundations/filters/logging-account-only"
-  global_only_tag  = "compliance/cis-aws-foundations/filters/global-resource-region-only"
+  compliance_standard_tag = "compliance/cis-aws-foundations/1.2"
+  logging_only_tag        = "compliance/cis-aws-foundations/filters/logging-account-only"
+  global_only_tag         = "compliance/cis-aws-foundations/filters/global-resource-region-only"
 
-  tagged_rules          = { for key, rule in local.rules_with_tags : key => rule if lookup(rule.tags, local.cis_1_2_tag, false) == true }
+  tagged_rules          = { for key, rule in local.rules_with_tags : key => rule if lookup(rule.tags, local.compliance_standard_tag, false) == true }
   logging_account_rules = { for key, rule in local.tagged_rules : key => rule if var.is_logging_account && lookup(rule.tags, local.logging_only_tag, false) }
   global_resource_rules = { for key, rule in local.tagged_rules : key => rule if var.is_global_resource_region && lookup(rule.tags, local.global_only_tag, false) }
   base_rules            = { for key, rule in local.tagged_rules : key => rule if(! lookup(rule.tags, local.logging_only_tag, false) && ! lookup(rule.tags, local.global_only_tag, false)) }

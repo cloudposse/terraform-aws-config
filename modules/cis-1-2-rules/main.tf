1da56ec8f758381999f2547c2943641ea42f4194
Matt Calhoun <matt@mattcalhoun.com>
2021-05-11 11:20:59 -0400

    update role to be global only

--- modules/cis-1-2-rules/main.tf
+++ modules/cis-1-2-rules/main.tf
@@ -9,13 +9,11 @@ locals {
   tagged_rules          = { for key, rule in local.rules_with_tags : key => rule if lookup(rule.tags, local.compliance_standard_tag, false) == true }
   logging_account_rules = { for key, rule in local.tagged_rules : key => rule if var.is_logging_account && lookup(rule.tags, local.logging_only_tag, false) }
   global_resource_rules = { for key, rule in local.tagged_rules : key => rule if var.is_global_resource_region && lookup(rule.tags, local.global_only_tag, false) }
-  base_rules            = { for key, rule in local.tagged_rules : key => rule if(! lookup(rule.tags, local.logging_only_tag, false) && ! lookup(rule.tags, local.global_only_tag, false)) }
+  base_rules            = { for key, rule in local.tagged_rules : key => rule if(!lookup(rule.tags, local.logging_only_tag, false) && !lookup(rule.tags, local.global_only_tag, false)) }
   all_rules             = merge(local.base_rules, local.logging_account_rules, local.global_resource_rules)
 
-  params = {
-    "iam-policy-in-use" : {
-      "policyARN" : var.support_policy_arn
-    }
+  base_params = {
+
     "s3-bucket-logging-enabled" : {
       "targetBucket" : var.cloudtrail_bucket_name
     }
@@ -23,6 +21,14 @@ locals {
       "s3BucketName" : var.cloudtrail_bucket_name
     }
   }
+
+  global_resource_params = {
+    "iam-policy-in-use" : {
+      "policyARN" : var.support_policy_arn
+    }
+  }
+
+  params = var.is_global_resource_region ? merge(local.base_params, local.global_resource_params) : local.base_params
 
   enabled_rules = { for key, rule in local.all_rules : key => {
     description      = rule.description,

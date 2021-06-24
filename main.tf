76a535f8b84e42855d3b23c85c11b48ec6d0b078
anandg112 <anandg112@gmail.com>
2021-06-24 15:06:04 -0400

    fix: remove eks cluster from recording for now

--- main.tf
+++ main.tf
@@ -16,10 +16,11 @@ resource "aws_config_configuration_recorder" "recorder" {
   recording_group {
     all_supported                 = false
     include_global_resource_types = local.is_global_recorder_region
-    resource_types = ["AWS::EC2::Host", "AWS::EC2::Instance", "AWS::EC2::VPC", "AWS::EKS::Cluster",
+    resource_types = ["AWS::EC2::Host", "AWS::EC2::Instance", "AWS::EC2::VPC",
       "AWS::ElasticLoadBalancingV2::LoadBalancer", "AWS::IAM::Group", "AWS::IAM::Policy",
       "AWS::IAM::Role", "AWS::Lambda::Function", "AWS::RDS::DBCluster", "AWS::RDS::DBInstance",
-    "AWS::S3::AccountPublicAccessBlock", "AWS::S3::Bucket", "AWS::EC2::Volume", "AWS::EC2::SecurityGroup", "AWS::CloudTrail::Trail", "AWS::KMS::Key"]
+      "AWS::S3::AccountPublicAccessBlock", "AWS::S3::Bucket", "AWS::EC2::Volume",
+    "AWS::EC2::SecurityGroup", "AWS::CloudTrail::Trail", "AWS::KMS::Key"]
   }
 }
 

8408ea2eeeeb3ca43291280ee44c42e12b156df9
anandg112 <anandg112@gmail.com>
2021-06-24 12:13:29 -0400

    feat: add more resource types to record

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_config_configuration_recorder" "recorder" {
     resource_types = ["AWS::EC2::Host", " AWS::EC2::Instance", "AWS::EC2::VPC", "AWS::EKS::Cluster",
       "AWS::ElasticLoadBalancingV2::LoadBalancer", "AWS::IAM::Group", "AWS::IAM::Policy",
       "AWS::IAM::Role", "AWS::Lambda::Function", "AWS::RDS::DBCluster", "AWS::RDS::DBInstance",
-    " AWS::S3::AccountPublicAccessBlock", "AWS::S3::Bucket", "AWS::EC2::Volume"]
+    " AWS::S3::AccountPublicAccessBlock", "AWS::S3::Bucket", "AWS::EC2::Volume", "AWS::EC2::SecurityGroup", "AWS::CloudTrail::Trail", "AWS::KMS::Key"]
   }
 }
 

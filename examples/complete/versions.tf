95c15540b6bfaf88a590956c4b9584de12209e29
Max Lobur <max_lobur@outlook.com>
2023-06-14 19:38:12 +0300

    Support AWS Provider V5

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.0"
 
   required_providers {
     local = {
@@ -8,7 +8,7 @@ terraform {
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2"
+      version = ">= 5.0"
     }
   }
 }

9cbefc9c311ae4c99231e0c49ca2c0d0c00f4842
boris-dyga-SM <123933753+boris-dyga-SM@users.noreply.github.com>
2024-01-22 18:30:31 +0200

    Added the option to use access tokens (#80)
    That feature allows to access private GitHub repos, where custom
    conformance packs could be stored
    
    Version constraints added to the conformance pack submodule to satisfy the tflint

--- modules/conformance-pack/main.tf
+++ modules/conformance-pack/main.tf
@@ -13,5 +13,13 @@ resource "aws_config_conformance_pack" "default" {
 }
 
 data "http" "conformance_pack" {
-  url = var.conformance_pack
+  /*
+  To access a public GitHub repo the following URL is used:
+    https://raw.githubusercontent.com/<owner>/<repo>/<branch>/<path_to_file>
+  
+  To access a private GitHub repo an access token with appropriate permissions should be generated first and then provided in the url:
+    https://<private_access_token>@raw.githubusercontent.com/<owner>/<repo>/<branch>/<path_to_file>
+  */
+
+  url = var.access_token == "" ? var.conformance_pack : "${split("://", var.conformance_pack)[0]}://${var.access_token}@${split("://", var.conformance_pack)[1]}"
 }

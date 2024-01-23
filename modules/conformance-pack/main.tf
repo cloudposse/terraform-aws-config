2ab02c7f6db78951f5dd770b0309b7c13623a73b
Boris Dyga <boris.dyga@savvymoney.com>
2024-01-23 14:19:15 +0200

    The access token is now passed in a http header This is done to avoid exposure as the data.http.id (which contains the URL) in the logs.

--- modules/conformance-pack/main.tf
+++ modules/conformance-pack/main.tf
@@ -13,13 +13,7 @@ resource "aws_config_conformance_pack" "default" {
 }
 
 data "http" "conformance_pack" {
-  /*
-  To access a public GitHub repo the following URL is used:
-    https://raw.githubusercontent.com/<owner>/<repo>/<branch>/<path_to_file>
-  
-  To access a private GitHub repo an access token with appropriate permissions should be generated first and then provided in the url:
-    https://<private_access_token>@raw.githubusercontent.com/<owner>/<repo>/<branch>/<path_to_file>
-  */
 
-  url = var.access_token == "" ? var.conformance_pack : "${split("://", var.conformance_pack)[0]}://${var.access_token}@${split("://", var.conformance_pack)[1]}"
+  url             = var.conformance_pack
+  request_headers = var.access_token == "" ? {} : { Authorization = "token ${var.access_token}" }
 }
